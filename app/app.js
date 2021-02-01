499c41203cfe55cd3bbb1b15e53896a0e1bfa6ff
ccrakel <crakel528@gmail.com>
2021-02-01 19:32:56 +0900

    2021 02 01

--- app/app.js
+++ app/app.js
@@ -18,6 +18,7 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = 3000;
@@ -27,11 +28,13 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); // HTML과 비슷
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// URL 한글, 공백 문자 인식 해결
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 app.use(express.static(`${__dirname}/src/public`)) // 정적 경로 추가 
 // dirname -> 현재 위치 
 
-app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
-
 module.exports = app;
 
-// npm init -y(자동완성)
+// npm init -y(자동완성) -> package.json 생
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,45 @@
 "use strict";
+// controller file
+const output = {
+	home: (req, res) => {
+		// 렌더링 기능
+		res.render("home/index");
+	},
 
-const home = (req, res) => {
-        // 기능
-        res.render("home/index");
+	login: (req, res) => {
+		res.render("home/login");
+	},
 };
 
-const login = (req, res) => {
-        res.render("home/login");
-};
+const users = {
+	id: ["crakel", "root", "temp"],
+	pw: ["1234", "1234", "123456"]
+	
+}
 
+const process = {
+	login: (req, res) => {
+		const id = req.body.id,
+			pw = req.body.pw
+	
+		if (users.id.includes(id)) {
+			const idx = users.id.indexOf(id);
+			if (users.pw[idx] === pw) {
+				return res.json({
+					success: true,
+				});
+			}
+		}
+
+		return res.json({
+			success: false,
+			msg: "로그인에 실패하였습니다.",
+		});
+	},
+}
 // object key 하나만 입력 -> 키와 같은 value로 넣어줌 (ES6)
 module.exports = {
-    home,
-    login, 
+	output,
+	process,
 };
 
